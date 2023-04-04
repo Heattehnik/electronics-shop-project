499b4ebdc70c8ecc6ead8da295c4146aba3f3a31
roman <gadjka@mail.ru>
2023-04-04 13:51:17 +0500

    Solved

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item('Лопата', 200, 40)
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 8000
+
+
+def test_apply_discount(item):
+    item.apply_discount()
+    assert item.price == 170
